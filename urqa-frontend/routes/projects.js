fd5451449e0737d0df9c356bd0870114373bb870
bongster <bongster88@gmail.con>
2015-02-20 13:03:19 +0900

    add project url mapping

--- urqa-frontend/routes/projects.js
+++ urqa-frontend/routes/projects.js
@@ -6,4 +6,36 @@ router.get('/', function(req, res, next) {
   res.render('projects/index');
 });
 
+
+/*
+ * urqa/project/registration$ url mapping
+*/
+router.post('/', function(req, res, next) {
+  res.render('projects/index');
+});
+
+/*
+ * urqa/project/(?P<apikey>.{8})/$ url mapping
+*/
+router.get('/:apikey', function(req, res, next) {
+    console.log(req.params.apikey);
+  res.render('projects/index');
+});
+
+/*
+ * ^urqa/project/(?P<apikey>.{8})/delete$' url mapping
+*/
+router.delete('/:apikey', function(req, res, next) {
+    console.log(req.params.apikey);
+  res.render('projects/index');
+});
+
+/*
+ * ^urqa/project/(?P<apikey>.{8})/modify$' url mapping
+*/
+router.put('/:apikey', function(req, res, next) {
+    console.log(req.params.apikey);
+  res.render('projects/index');
+});
+
 module.exports = router;
