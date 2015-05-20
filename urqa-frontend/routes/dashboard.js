5a0fbcc0f0bae369e98319073f339e3ee9cb35e8
카르아나 <mse201@gmail.com>
2015-05-20 23:33:37 +0900

    대쉬보드 폴더 구조 변경
    * 기존 콘텐츠만 나누었던 구조에서 스크립트도 분리하도록 수

--- urqa-frontend/routes/dashboard.js
+++ urqa-frontend/routes/dashboard.js
@@ -1,39 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var getViewContainer = function(defaultPath) {
+    return {
+        mainContainer: defaultPath + "/index",
+        scriptContainer: defaultPath + "/scripts"
+    };
+};
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/dashboard"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/dashboard"));
 });
 
 router.get('/error', function(req, res) {
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/error"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/error"));
 });
 
 router.get('/error/:idx?', function(req, res) {
   var id = req.params.id;
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/detail"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/detail"));
 });
 
 router.get('/statistics', function(req, res) {
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/statistics"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/statistics"));
 });
 
 
 router.get('/setting', function(req, res) {
   var id = req.params.id;
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/setting/general"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/setting/general"));
 });
 
 router.get('/setting/symbolicate', function(req, res) {
   var id = req.params.id;
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/setting/symbolicate"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/setting/symbolicate"));
 });
 
 router.get('/setting/viewer', function(req, res) {
   var id = req.params.id;
-  res.render('layout/dashboard/layout', {mainContainer : "../../dashboard/setting/viewer"});
+  res.render('layout/dashboard/layout', getViewContainer("../../dashboard/setting/viewer"));
 });
+
+
+
 
 
 module.exports = router;
