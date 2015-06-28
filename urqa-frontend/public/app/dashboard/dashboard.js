d3d7ba5d678979adf0a36d5f32f17e39a597b0d2
카르아나 <mse201@gmail.com>
2015-06-28 23:35:26 +0900

    [URQAIO-167] 대쉬보드 라인 그래프 영역 작업
    * 대쉬보드 라인 그래프 영역 작업
    * apikey 공유를 위한 서비스 구

--- urqa-frontend/public/app/dashboard/dashboard.js
+++ urqa-frontend/public/app/dashboard/dashboard.js
@@ -7,10 +7,110 @@ angular.module("app")
 
     })
 
-    .controller("DashBoardInfoController", function ($scope) {
+    .controller("DashBoardController", function($scope, ApiKeyService){
 
+        $scope.initApiKey = function(apiKey) {
+            ApiKeyService.setApiKey(apiKey);
+        }
     })
 
+    .controller("DashBoardDailyGraphController", function ($scope, $element, DashboardErrorDailyLoader) {
+
+
+        $scope.elementJquery = $($element);
+
+        var dailyChart = $scope.elementJquery.find("#daily-visit-chart");
+
+        var options = {
+            grid: {
+                backgroundColor: {
+                    colors: ["#fff", "#fff"]
+                },
+                borderWidth: 0,
+                borderColor: "#f0f0f0",
+                margin: 0,
+                minBorderMargin: 0,
+                labelMargin: 20,
+                hoverable: true,
+                clickable: true
+            },
+            // Tooltip
+            tooltip: true,
+            tooltipOpts: {
+                content: "%s X: %x Y: %y",
+                shifts: {
+                    x: -60,
+                    y: 25
+                },
+                defaultTheme: false
+            },
+
+            legend: {
+                labelBoxBorderColor: "#ccc",
+                show: false,
+                noColumns: 0
+            },
+            series: {
+                stack: true,
+                shadowSize: 0,
+                highlightColor: 'rgba(30,120,120,.5)'
+
+            },
+            xaxis: {
+                tickLength: 0,
+                tickDecimals: 0,
+                show: true,
+                min: 2,
+
+                font: {
+
+                    style: "normal",
+
+
+                    color: "#666666"
+                }
+            },
+            yaxis: {
+                ticks: 3,
+                tickDecimals: 0,
+                show: true,
+                tickColor: "#f0f0f0",
+                font: {
+
+                    style: "normal",
+
+
+                    color: "#666666"
+                }
+            },
+            points: {
+                show: true,
+                radius: 2,
+                symbol: "circle"
+            },
+            colors: ["#87cfcb", "#48a9a7"]
+        };
+
+        DashboardErrorDailyLoader().success(function(response){
+            var data = ([{
+                label: "Too",
+                data: response.data,
+                lines: {
+                    show: true,
+                    fill: true,
+                    lineWidth: 2,
+                    fillColor: {
+                        colors: ["rgba(255,255,255,.1)", "rgba(160,220,220,.8)"]
+                    }
+                }
+            }]);
+
+            $scope.$dailyChart = $.plot(dailyChart, data, options);
+        });
+
+
+
+    })
     .controller("DashboardErrorGraphController", function ($scope, $element,  DashboardErrorPieGraphLoader) {
 
         $scope.elementJquery = $($element);
