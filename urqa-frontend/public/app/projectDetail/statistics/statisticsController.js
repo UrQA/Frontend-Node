370498d770bbffea0650341084fa7da49e91d3a6
카르아나 <mse201@gmail.com>
2015-07-12 23:58:12 +0900

    월드맵 통계 위치 수정
    * 월드맵 통계 위치를 가장 하단으로 수

--- urqa-frontend/public/app/projectDetail/statistics/statisticsController.js
+++ urqa-frontend/public/app/projectDetail/statistics/statisticsController.js
@@ -150,7 +150,7 @@ angular.module("app")
             },
 
             color: {
-                pattern: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5']
+                pattern: [ '#9edae5']
             },
             legend: {
                 show: false
@@ -163,27 +163,43 @@ angular.module("app")
         });
     })
     .controller("classErrorRateController", function($scope) {
+        $scope.data =  [
+            {value: 17.6, label: 'com.android.volley.toolbox.d'},
+            {value: 13.7, label: 'at droom.sleepIfUCan.activity.af (:211)'},
+            {value: 11, label: 'droom.sleepIfUCan.activity.af' },
+            {value: 8.7, label: 'android.database.sqlite.SQLiteStatement'},
+            {value: 8.5, label: 'android.content.res.Resources'},
+            {value: 5.3, label: 'android.os.BinderProxy' },
+            {value: 4.5, label: 'at android.widget.NumberPicker$ChangeCurrentByOneFromLongPressCommand' },
+            {value: 4.3, label: 'at android.content.res.Resources' },
+            {value: 4, label: 'android.database.sqlite.SQLiteStatement' },
+            {value: 2, label: 'android.database.sqlite.SQLiteConnection' },
+            {value: 1.6, label: 'droom.sleepIfUCan.activity.SelectPictureActivity' },
+            {value: 18.8, label: 'Others' },
+        ];
+
+        $scope.getItemClass = function(data) {
+            if(data.label === 'Others') {
+                return 'progress-bar-info';
+            }
+
+            if(data.value > 15) {
+                return 'progress-bar-danger';
+            } else if(data.value > 10){
+                return 'progress-bar-warning';
+            } else {
+                return 'progress-bar-info';
+            }
+        }
+
         Morris.Donut({
             element: 'graph-donut',
-            data: [
-                {value: 17.6, label: 'com.android.volley.toolbox.d', formatted: '17.6%' },
-                {value: 13.7, label: 'at droom.sleepIfUCan.activity.af (:211)', formatted: '13.7%' },
-                {value: 11, label: 'droom.sleepIfUCan.activity.af', formatted: '11%' },
-                {value: 8.7, label: 'android.database.sqlite.SQLiteStatement', formatted: '8.7%' },
-                {value: 8.5, label: 'android.content.res.Resources', formatted: '8.5%' },
-                {value: 5.3, label: 'android.os.BinderProxy', formatted: '5.3%' },
-                {value: 4.5, label: 'at android.widget.NumberPicker$ChangeCurrentByOneFromLongPressCommand', formatted: '4.5%' },
-                {value: 4.3, label: 'at android.content.res.Resources', formatted: '4.3%' },
-                {value: 4, label: 'android.database.sqlite.SQLiteStatement', formatted: '4%' },
-                {value: 2, label: 'android.database.sqlite.SQLiteConnection', formatted: '2%' },
-                {value: 1.6, label: 'droom.sleepIfUCan.activity.SelectPictureActivity', formatted: '1.6%' },
-                {value: 18.8, label: 'Others', formatted: '18.8%' },
-            ],
+            data:$scope.data,
             backgroundColor: '#fff',
             labelColor: '#1fb5ac',
             colors: [
                 '#E67A77','#D9DD81','#79D1CF','#95D7BB'
             ],
-            formatter: function (x, data) { return data.formatted; }
+            formatter: function (x, data) { return data.value + "%"; }
         });
     });
